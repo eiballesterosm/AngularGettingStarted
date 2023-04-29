7387f7c05d7211d2fd386c3c826801b0bcbd8921
eiballesterosm <eduballesteros@gmail.com>
2023-04-28 23:49:32 -0500

    Implements OnInit event

--- src/app/products/product-list-component.ts
+++ src/app/products/product-list-component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./IProduct";
 
 @Component({
@@ -6,7 +6,11 @@ import { IProduct } from "./IProduct";
     templateUrl: './product-list.component.html',
     styleUrls:['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+
+    ngOnInit(): void {
+        console.log('ngOnInit');
+    }
 
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
