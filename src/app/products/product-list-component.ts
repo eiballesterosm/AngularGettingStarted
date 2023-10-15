fd957133e8f0966b7831b20b3cc34b62548f6681
eiballesterosm <eduballesteros@gmail.com>
2023-10-15 18:20:53 -0500

    Add the Observable to the productService

--- src/app/products/product-list-component.ts
+++ src/app/products/product-list-component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./IProduct";
 import { ProductService } from "./product.service";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: 'pm-products',
@@ -9,22 +10,32 @@ import { ProductService } from "./product.service";
 })
 export class ProductListComponent implements OnInit {
 
-    constructor(private productService: ProductService) {
-
-    }
-
-    ngOnInit(): void {
-        console.log('ngOnInit');
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
-        //Default listFilter value
-        //this.listFilter = 'cart';
-    }
-
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 4;
     showImage: boolean = false;
+    errorMessage: string = '';
+    sub!: Subscription;
+
+    constructor(private productService: ProductService) {
+    }
+
+    ngOnInit(): void {
+        console.log('ngOnInit');        
+        this.sub = this.productService.getProducts().subscribe({
+            next: pr => {
+                this.products = pr;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err
+        });
+        //Default listFilter value
+        //this.listFilter = 'cart';
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
+    }
 
     private _listFilter: string = '';
     get listFilter(): string {
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,53 +1,32 @@
 import { Injectable } from "@angular/core";
 import { IProduct } from "./IProduct";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable, catchError, tap, throwError } from "rxjs";
 
 @Injectable({
-    providedIn : 'root'
+    providedIn: 'root'
 })
 export class ProductService {
 
-    getProducts(): IProduct[] {
-        return [
-            {
-                "productId": 1,
-                "productName": "Garden Cart",
-                "productCode": "CODE-0001",
-                "releaseDate": "20220101",
-                "description": 'Product description',
-                "price": 32.99,
-                "starRating": 1.2,
-                "imageUrl": "assets/images/leaf_rake.png"
-            },
-            {
-                "productId": 2,
-                "productName": "Product 2",
-                "productCode": "CODE-0002",
-                "releaseDate": "20220101",
-                "description": 'Product description',
-                "price": 178.799,
-                "starRating": 4.2,
-                "imageUrl": "assets/images/garden_cart.png"
-            },
-            {
-                "productId": 3,
-                "productName": "Product 3",
-                "productCode": "CODE-0003",
-                "releaseDate": "20220101",
-                "description": 'Product description',
-                "price": 2.869,
-                "starRating": 3.2,
-                "imageUrl": "assets/images/hammer.png"
-            },
-            {
-                "productId": 4,
-                "productName": "Product 4",
-                "productCode": "CODE-0004",
-                "releaseDate": "20220101",
-                "description": 'Product description 4',
-                "price": 2.869,
-                "starRating": 3.7,
-                "imageUrl": "assets/images/saw.png"
-            },
-        ];
+    private productUrl = 'api/products/products.json';
+    constructor(private http: HttpClient) { }
+
+    getProducts(): Observable<IProduct[]> {
+        return this.http.get<IProduct[]>(this.productUrl).pipe(
+            tap(data => console.log('All', JSON.stringify(data))),
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError(err: HttpErrorResponse) {
+        let errorMessage = '';
+        if (err.error instanceof ErrorEvent) {
+            errorMessage = `An error occurred: ${err.error.message}`;
+        }
+        else {
+            errorMessage = `Server returne code: ${err.status}, error message is: ${err.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(() => errorMessage);
     }
 }
